ed2c8ac7faa162fdac26088869b6f086663f0a7e
Jackie Chen <support+git@jackiechen.org>
2023-07-14 17:27:59 +1000

    User defined data model for creating item

--- lib/ald-stack.ts
+++ lib/ald-stack.ts
@@ -16,21 +16,41 @@ export class ALDStack extends cdk.Stack {
   constructor(app: Construct, id: string, props: ALDStackProps) {
     super(app, id, props);
 
+    // DynamoDB
     const scheduleTable = new DynamoDb(this, "DynamoDb", {
       name: props.tableName,
       partitionKey: props.partitionKey,
     }).table;
 
+    // Lambdas
     const scheduleLambda = new Lambda(this, "Lambda", {
       tableName: props.tableName,
       partitionKey: props.partitionKey,
     });
 
+    // API
     const scheduleApi = new ApiGateway(this, "ApiGateway", {
       name: props.tableName,
       stageName: props.stageName,
     }).api;
 
+    // Data model
+    const itemModel: apiGw.Model = scheduleApi.addModel("ItemModel", {
+      schema: {
+        type: apiGw.JsonSchemaType.OBJECT,
+        properties: {
+          id: {
+            type: apiGw.JsonSchemaType.STRING,
+          },
+          topic: {
+            type: apiGw.JsonSchemaType.STRING,
+          },
+        },
+        required: ["id"],
+      },
+    });
+
+    // API usage plan
     const plan = scheduleApi.addUsagePlan("UsagePlan", {
       name: "Easy",
       throttle: {
@@ -39,22 +59,29 @@ export class ALDStack extends cdk.Stack {
       },
     });
 
+    // API key
     const key = scheduleApi.addApiKey("ApiKey");
     plan.addApiKey(key);
     plan.addApiStage({ stage: scheduleApi.deploymentStage });
 
+    // Lambda access permission to DynamoDB
     scheduleTable.grantReadWriteData(scheduleLambda.createItem);
     scheduleTable.grantReadData(scheduleLambda.getItem);
     scheduleTable.grantReadData(scheduleLambda.getItems);
 
+    // API gateway to Lambda integration
     const createItem = new apiGw.LambdaIntegration(scheduleLambda.createItem);
-
     const getItem = new apiGw.LambdaIntegration(scheduleLambda.getItem);
-
     const getItems = new apiGw.LambdaIntegration(scheduleLambda.getItems);
 
+    // API path and associated backend Lambda
     const items = scheduleApi.root.addResource("items");
-    items.addMethod("GET", getItems, { apiKeyRequired: true });
+    items.addMethod("GET", getItems, {
+      apiKeyRequired: true,
+      requestModels: {
+        "application/json": itemModel,
+      },
+    });
     addCorsOptions(items);
 
     const item = scheduleApi.root.addResource("item");
